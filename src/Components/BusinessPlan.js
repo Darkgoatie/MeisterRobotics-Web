5c5e20860f57b4ac566817935c63a36e097cd74e
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2021-12-04 23:12:42 +0300

    Remove Projects and Timelane sections, add Businessplan and sponsorship sections

---
+++ src/Components/BusinessPlan.js
@@ -0,0 +1,21 @@
+const Projects = () => {
+    return ( 
+        
+    <div class="section dark" id="businessplan">
+        <div class="jumbotron">
+           <h1 class="display-4">Business Plan: Who are we, what do we do?</h1>
+           <br />
+           <p class="lead"><div class="dropdown">
+  <button class="btn center-align btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Business Plan Info
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a className="dropdown-item" href="https://cdn.discordapp.com/attachments/916762048606650378/916780683609911306/Business_Plan_EN.pdf">Business Plan EN</a></li>
+  </ul>
+</div></p>
+        </div>
+        </div>
+     );
+}
+ 
+export default Projects;
---
+++ src/Components/Sponsorships.js
@@ -0,0 +1,22 @@
+const Timeline = () => {
+    return ( 
+        
+    <div class="section" id="sponsorship">
+        <div class="jumbotron">
+           <h1 class="display-4">Sponsorships: Become a sponsor!</h1>
+           <br />
+           <p class="lead"><div class="dropdown">
+  <button class="btn center-align btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Sponsorship Info
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a className="dropdown-item" href="https://cdn.discordapp.com/attachments/916762048606650378/916762178089000960/SponsorlukDosyasi.pdf">Sponsorship TR</a></li>
+    <li><a className="dropdown-item" href="https://cdn.discordapp.com/attachments/916762048606650378/916777050377965660/SponsorshipFile.pdf">Sponsorship EN</a></li>
+  </ul>
+</div></p>
+        </div>
+    </div>
+     );
+}
+ 
+export default Timeline;
