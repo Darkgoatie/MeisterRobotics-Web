fdf274acee29d0b2f6bbfa4e23c29b40961cfe94
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2021-12-04 23:11:56 +0300

    Fix navbar to stay on top while scrolling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return ( 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+        <nav className="fixed-top navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
     <div className="container-fluid">
         <a className="navbar-brand" href="#"><img alt="icon" height="32px" src="/MeisterRobotics-modified.png"></img> Meister Robotics</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
@@ -11,8 +11,8 @@ const Navbar = () => {
                 <a className="nav-item nav-link" href="#">Home</a>
                 <a className="nav-item nav-link" href="#thanks">Special Thanks</a>
                 <a className="nav-item nav-link" href="#about">About</a>
-                <a className="nav-item nav-link" href="#projects">Projects</a>
-                <a className="nav-item nav-link" href="#timeline">Timeline</a>
+                <a className="nav-item nav-link" href="#projects">Business Plan</a>
+                <a className="nav-item nav-link" href="#timeline">Sponsorships</a>
             </div>
         </div>
     </div>
